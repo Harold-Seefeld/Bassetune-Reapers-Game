4f0512829f9c648fc85e4994ae4358faad6406ba
Harold <seefeld.harold@gmail.com>
2015-01-06 14:55:33 +1100

    Update hero and boss menus

---
+++ Assets/Scripts/UI/Main Menu/Character Management/ButtonSet.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonSet : MonoBehaviour, IPointerClickHandler {
+
+
+	public void OnEnable()
+	{
+
+	}
+
+	public void OnPointerClick (PointerEventData eventData)
+	{
+		GameObject parentObject = transform.parent.gameObject;
+		Button[] buttons = parentObject.GetComponentsInChildren<Button>() as Button[];
+
+		for (int i = 0; i < buttons.Length; i++)
+		{
+			buttons[i].interactable = true;
+		}
+
+		gameObject.GetComponent<Button>().interactable = false;
+	}
+	
+}
