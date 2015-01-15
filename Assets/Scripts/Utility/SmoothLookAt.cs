a5222faef5385ad6487ad7f39fa2f1af63d635b4
FS02 <Fs_02@mail.com>
2015-01-15 11:11:55 +0700

    Porting SmoothFollow and SmoothLookAt from Javascript to C# Mono

---
+++ Assets/Scripts/Utility/SmoothLookAt.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Original Version : https://gist.github.com/tracend/893624
+ */
+
+public class SmoothLookAt : MonoBehaviour {
+	public Transform target = null;
+	public float damping = 6f;
+	public bool smooth = true;
+
+	void Start () {
+		if (rigidbody)
+			rigidbody.freezeRotation = true;
+	}
+	
+	void LateUpdate () {
+		if (target){
+			if (smooth){
+				// Look at and damping the rotation
+				Quaternion rotation = Quaternion.LookRotation(target.position - transform.position);
+				transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * damping);
+			} else {
+				// Use internal function
+				transform.LookAt(target);
+			}
+		}
+	}
+}
