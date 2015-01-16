070ce97a0fd7aa012aae98d375ea91f114c06270
Harold <seefeld.harold@gmail.com>
2015-01-16 21:07:02 +1100

    Add 4 traps and health ui script

---
+++ Assets/Scripts/UI/Gameplay/HealthBar.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+	
+	public LifeSystem lifeSystem;
+	public Text lifeText;
+	public Slider healthSlider;
+	
+	void Update() 
+	{
+		// Set value each frame
+		healthSlider.maxValue = lifeSystem.maxHealth;
+		healthSlider.minValue = 0;
+		
+		// Set the value to the current health
+		healthSlider.value = lifeSystem.health;
+		
+		// Set value each frame
+		lifeText.text = Mathf.Round(lifeSystem.health).ToString();
+	}
+}
