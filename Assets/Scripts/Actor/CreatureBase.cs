ef49d0e99adf31d11ac5d31685fff085bbe8da66
Harold <seefeld.harold@gmail.com>
2015-01-21 20:29:29 +1100

    Revert to 4d03366f72

--- Assets/Scripts/Actor/CreatureBase.cs
+++ Assets/Scripts/Actor/CreatureBase.cs
@@ -3,41 +3,76 @@ using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
-public class CreatureController : MonoBehaviour {
-	public List<Transform> target;
+/*
+ * This is base Actor for all creature  actor.
+ * If you overriding Unity function from this class, don't forget to call Base[FunctionToOverride] before writing your scripts.
+ * Remember base.[FunctionToOverride] won't work due to unity reflection architectures.
+ */
+
+[AddComponentMenu("Actor/CreatureBase")]
+public class CreatureBase : MonoBehaviour {
+	// List of patrol waypoint for character, leave it empty to use random waypoint
 	public Transform [] patrolWaypoints;
-	public Transform randomAnchor;
+	// Anchor of randomization waypoint, it act as chain that makes random patrol only randomized in certain area
+	public Transform creatureAnchor;
+	// Max length of anchor chain
+	public float anchorRange = 20f;
+	// Sight distance of creatures
 	public float sightDistance = 10f;
+	// Attack range of creatures
 	public float attackRange = 1f;
+	// Idle time between waypoint
 	public float idleTime = 1f;
+	// Time needed to do attack movement
 	public float attackTime = 0.1f;
+	// Cool down time between attack movement
 	public float cooldownTime = 1.5f;
 
+	// This hold all characters that currently inside creature sight
+	List<Transform> target = new List<Transform> ();
+	// Agent for pathfinding and movement
 	NavMeshAgent agent;
+	// Attacking flag
 	bool isAttacking = false;
+	// Track current waypoint index
 	int currentWaypoint = 0;
+	// Timer for used internally
 	float timer = 0f;
-	
+	// Debug AI
 	public GameObject debugLabel;
 	Text debugLabelText;
 
-	// Use this for initialization
+	// Unity Start function
 	void Start () {
-		agent = GetComponent<NavMeshAgent> ();
+		BaseStart ();
+	}
 
+	// Base function of Start
+	protected void BaseStart(){
+		agent = GetComponent<NavMeshAgent> ();
+		agent.stoppingDistance = attackRange;
+		
 		#if UNITY_EDITOR
 		debugLabel = Instantiate (debugLabel) as GameObject;
 		debugLabelText = debugLabel.GetComponent<Text> ();
 		debugLabel.transform.SetParent (GameObject.Find ("InGameCanvas").transform);
+		#else
+		debugLabel.SetActive(false);
+		debugLabelText.text = "";
 		#endif
 	}
-	
-	// Update is called once per frame
+
+	// Unity Update function
 	void Update () {
+		BaseUpdate ();
+	}
+
+	// Base function of update
+	protected void BaseUpdate(){
 		if (target.Count > 0) {
 			if (Vector3.Distance(target[0].position, transform.position) > attackRange){
 				Chase();
-
+				
 				#if UNITY_EDITOR
 				debugLabelText.text = "Chasing";
 				#endif
@@ -50,19 +85,19 @@ public class CreatureController : MonoBehaviour {
 			}
 		} else {
 			Patrol();
-		
+			
 			#if UNITY_EDITOR
 			debugLabelText.text = "Patrol";
 			#endif
 		}
-
+		
 		#if UNITY_EDITOR
 		Vector2 screenPoint = RectTransformUtility.WorldToScreenPoint (Camera.main, gameObject.transform.position);
 		debugLabelText.rectTransform.anchoredPosition = screenPoint - debugLabelText.canvas.GetComponent<RectTransform> ().sizeDelta / 2f;
 		#endif
 	}
 	
-	bool Attack(){
+	protected virtual bool Attack(){
 		if ((isAttacking && timer < attackTime) || (!isAttacking && timer < cooldownTime)){
 			timer += Time.deltaTime;
 			return isAttacking;
@@ -72,13 +107,10 @@ public class CreatureController : MonoBehaviour {
 		timer = 0f;
 		isAttacking = !isAttacking;
 
-		// (dmongs) knight is attacked
-		Knight.Instance.SetAniParamAttacked( isAttacking );
-
 		return isAttacking;
 	}
 
-	void Chase(){
+	protected virtual void Chase(){
 		if (timer < 0.25f){
 			timer += Time.deltaTime;
 			return;
@@ -87,29 +119,29 @@ public class CreatureController : MonoBehaviour {
 		agent.SetDestination(target[0].position);
 	}
 
-	void Patrol(){
-		if (agent.remainingDistance <= float.Epsilon){
+	protected virtual void Patrol(){
+		if (agent.remainingDistance <= agent.stoppingDistance){
 			if (timer > idleTime){
 				if (patrolWaypoints.Length > 1){
 					currentWaypoint = (currentWaypoint + 1) < patrolWaypoints.Length ? currentWaypoint + 1 : 0;
 					agent.SetDestination(patrolWaypoints[currentWaypoint].position);
 					timer = 0f;
 				} else {
-					if (!randomAnchor){
-						Debug.LogWarning("Warning, Random Anchor is not set");
+					if (!creatureAnchor){
+						Debug.LogWarning("Warning, Creature Anchor is not set");
 						return;
 					}
 					RaycastHit rHit;
-					Vector3 randomPosition = Random.insideUnitSphere * 20;
+					Vector3 randomPosition = Random.insideUnitSphere * anchorRange;
 					randomPosition.Set(randomPosition.x, 0, randomPosition.z);
-					if (Physics.Raycast (randomAnchor.position, randomPosition, out rHit)){
+					if (Physics.Raycast (creatureAnchor.position, randomPosition, out rHit)){
 						randomPosition = rHit.point;
 					} else {
-						randomPosition += randomAnchor.position;
+						randomPosition += creatureAnchor.position;
 					}
 
 					NavMeshHit hit;
-					NavMesh.SamplePosition(randomPosition, out hit, 20, 1);
+					NavMesh.SamplePosition(randomPosition, out hit, anchorRange, 1);
 					agent.SetDestination(hit.position);
 					timer = 0f;
 				}
@@ -120,12 +152,20 @@ public class CreatureController : MonoBehaviour {
 	}
 	
 	void OnTriggerEnter(Collider other) {
+		BaseOnTriggerEnter (other);
+	}
+
+	protected void BaseOnTriggerEnter(Collider other){
 		if (other.tag == "Knight"){
 			target.Add(other.transform);
 		}
 	}
 	
 	void OnTriggerExit(Collider other) {
+		BaseOnTriggerExit (other);
+	}
+
+	protected void BaseOnTriggerExit(Collider other){
 		if (other.tag == "Knight"){
 			target.Remove(other.transform);
 		}
---
+++ Assets/Scripts/UI/Login System/Register.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+public class Register : MonoBehaviour {
+
+	public InternetCalls calls;
+	public string Username;
+	public string Password;
+	public string Email;
+	public string ConfirmPassword;
+	public GUIStyle GUINormal;
+	public GUIStyle GUITitle;
+	public GUIStyle GUIErrors;
+	public GUIStyle GUIButton;
+	public string IPAddress;
+	private bool IsValidEmail(string strIn){return Regex.IsMatch(strIn, @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");}
+	
+	private void OnGUI(){
+		/*
+		 * WARNING : PLEASE DON'T USE OUTDATED AND EXPENSIVE UNITY GUI FUNCTION - SURYA
+		 */
+		
+		GUILayout.BeginArea (new Rect ((Screen.width / 2) + 94, (Screen.height / 2) - 40, 256, 24), GUIContent.none, "box");
+		GUILayout.Label ("Register", GUITitle);
+		GUILayout.EndArea ();
+		
+		// Register
+		
+		GUILayout.BeginArea (new Rect ((Screen.width / 2) + 30, (Screen.height / 2) - 10, 384, 256), GUIContent.none, "box");
+		GUILayout.BeginVertical();
+		GUILayout.Label ("Username", GUINormal);
+		Username = GUILayout.TextField (Username);
+		GUILayout.EndVertical ();
+
+		GUILayout.BeginVertical();
+		GUILayout.Label ("Email", GUINormal);
+		Email = GUILayout.TextField (Email);
+		GUILayout.EndVertical ();
+
+		GUILayout.BeginVertical();
+		GUILayout.Label ("Password", GUINormal);
+		Password = GUILayout.PasswordField (Password, "*"[0], 24);
+		GUILayout.EndVertical ();
+
+		GUILayout.BeginVertical();
+		GUILayout.Label ("Confirm Password", GUINormal);
+		ConfirmPassword = GUILayout.PasswordField (ConfirmPassword, "*"[0], 24);
+		GUILayout.EndVertical ();
+
+		if (Username.Length > 3){
+			if (IsValidEmail(Email)){
+				if (Password.Length < 6) {
+					GUILayout.Button ("Register");
+					GUILayout.Label ("Password must be 6-24 characters long.", GUIErrors);
+				}
+
+				else if (Password.Length >=6) {
+					if (Password == ConfirmPassword) {
+						if (GUILayout.Button ("Register")) {
+							calls.DoRegister (Username, Password, Email);
+					}
+
+					}else if (Password != ConfirmPassword) {
+						GUILayout.Button ("Register");
+						GUILayout.Label ("Passwords don't match.", GUIErrors);
+					}
+				}
+
+			}else if (!IsValidEmail(Email)){
+				GUILayout.Button ("Register");
+				GUILayout.Label ("Please enter a valid email.", GUIErrors);
+			}
+
+		}else if (Username.Length <= 3) {
+			GUILayout.Button ("Register");
+			GUILayout.Label ("Username must be 4-24 characters long.", GUIErrors);
+		}
+
+		GUILayout.Label (InternetCalls.registrationStatus, GUIErrors);
+		GUILayout.EndArea ();
+
+	}
+}
