c016efae03965305aff948fd5432cd8fded0534d
Antonin Bernardin <antonin.bernardin@gmail.com>
2015-01-22 20:55:34 +0100

    Socketio plugin implementation in a test scene (sandbox)

---
+++ Assets/Scripts/Multiplayer/SocketIOTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using SocketIO;
+
+public class SocketIOTest : MonoBehaviour {
+
+	private SocketIOComponent socket;
+
+	// Use this for initialization
+	void Start () {
+		GameObject go = GameObject.Find ("SocketIO");
+		socket = go.GetComponent<SocketIOComponent>();
+
+		socket.Connect ();
+
+		System.Threading.Thread.Sleep (1000);
+
+		socket.On ("boop", TestBoop);
+		socket.Emit ("beep");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void TestBoop(SocketIOEvent e){
+		Debug.Log("boop received");
+		socket.Emit ("beep");
+	}
+}
