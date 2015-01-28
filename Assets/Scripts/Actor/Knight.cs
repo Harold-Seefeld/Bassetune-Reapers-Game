b835b2f76dc83a5b73e1b117dd53dbdc37c61e21
Shadic1910 <mario.shadic.esteves@hotmail.com>
2015-01-28 10:47:51 +0000

    Changes to the creature and knight prefab, and implemented ability targeting. Please check the comments on the code if you are planning changes.

--- Assets/Scripts/Actor/Knight.cs
+++ Assets/Scripts/Actor/Knight.cs
@@ -11,13 +11,24 @@ public class Knight : PlayerBase {
 	const string AniMoveSpeedName = "MoveSpeed";
 	const string AniAttackedName = "Attacked";
 
+    //when this actor is using smart cast
+    public bool isUsingSmartCast;
+    //target actor of the current ability
+    public GameObject targetActor;
+    
+    //the ability index currently selected
+    public int currentAbility;
+
 	void Start () {
 		// Call base start function
 		BaseStart ();
-		
+
+        currentAbility = -1;
+
 		// initialize references
 		anim = GetComponent<Animator> ();
 		inGameCanvas = GameObject.Find ("Knight Canvas").GetComponent<InGameCanvas> ();
+        targetActor = gameObject;
 
 		for (int i = 0; i < abilities.Length; ++i){
 			if (abilities[i]){
@@ -35,6 +46,10 @@ public class Knight : PlayerBase {
 		anim.SetFloat (AniMoveSpeedName, agent.velocity.magnitude);
 		//if any pressed ability hotkey
 		OnPressedAbilityHotkey();
+        if (Input.GetMouseButtonDown(0))
+        {
+            MousePressed();
+        }
 	}
 	
 	void OnPressedAbilityHotkey()
@@ -42,8 +57,8 @@ public class Knight : PlayerBase {
 		//invokes the ability when the button is pressed
 		if (Input.GetAxis("Attack1") > 0)
 		{
-			if (abilities.Length > 0 && abilities[0]) 
-				abilities[0].Cast();
+            if (abilities.Length > 0 && abilities[0])
+                currentAbility = 0;
 
 			#if UNITY_EDITOR
 			debugLabelText.text = "Use Ability 1";
@@ -51,8 +66,8 @@ public class Knight : PlayerBase {
 		}
 		else if (Input.GetAxis("Attack2") > 0)
 		{
-			if (abilities.Length > 1 && abilities[1]) 
-				abilities[1].Cast();
+			if (abilities.Length > 1 && abilities[1])
+                currentAbility = 1;
 			
 			#if UNITY_EDITOR
 			debugLabelText.text = "Use Ability 2";
@@ -60,8 +75,8 @@ public class Knight : PlayerBase {
 		}
 		else if (Input.GetAxis("Attack3") > 0)
 		{
-			if (abilities.Length > 2 && abilities[2]) 
-				abilities[2].Cast();
+			if (abilities.Length > 2 && abilities[2])
+                currentAbility = 2;
 			
 			#if UNITY_EDITOR
 			debugLabelText.text = "Use Ability 3";
@@ -69,8 +84,8 @@ public class Knight : PlayerBase {
 		}
 		else if (Input.GetAxis("Attack4") > 0)
 		{
-			if (abilities.Length > 3 && abilities[3]) 
-				abilities[3].Cast();
+			if (abilities.Length > 3 && abilities[3])
+                currentAbility = 3;
 			
 			#if UNITY_EDITOR
 			debugLabelText.text = "Use Ability 4";
@@ -78,8 +93,8 @@ public class Knight : PlayerBase {
 		}
 		else if (Input.GetAxis("Attack5") > 0)
 		{
-			if (abilities.Length > 4 && abilities[4]) 
-				abilities[4].Cast();
+			if (abilities.Length > 4 && abilities[4])
+                currentAbility = 4;
 			
 			#if UNITY_EDITOR
 			debugLabelText.text = "Use Ability 5";
@@ -87,8 +102,8 @@ public class Knight : PlayerBase {
 		}
 		else if (Input.GetAxis("Attack6") > 0)
 		{
-			if (abilities.Length > 5 && abilities[5]) 
-				abilities[5].Cast();
+			if (abilities.Length > 5 && abilities[5])
+                currentAbility = 5;
 			
 			#if UNITY_EDITOR
 			debugLabelText.text = "Use Ability 6";
@@ -96,8 +111,8 @@ public class Knight : PlayerBase {
 		}
 		else if (Input.GetAxis("Attack7") > 0)
 		{
-			if (abilities.Length > 6 && abilities[6]) 
-				abilities[6].Cast();
+			if (abilities.Length > 6 && abilities[6])
+                currentAbility = 6;
 			
 			#if UNITY_EDITOR
 			debugLabelText.text = "Use Ability 7";
@@ -105,12 +120,56 @@ public class Knight : PlayerBase {
 		}
 		else if (Input.GetAxis("Attack8") > 0)
 		{
-			if (abilities.Length > 7 && abilities[7]) 
-				abilities[7].Cast();
+			if (abilities.Length > 7 && abilities[7])
+                currentAbility = 7;
 			
 			#if UNITY_EDITOR
 			debugLabelText.text = "Use Ability 8";
 			#endif
 		}
 	}
+
+    void MousePressed()
+    {
+        Debug.Log("Mouse Down");
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (targetActor != null)
+        {
+            targetActor.GetComponent<ParticleSystem>().enableEmission = false;
+        }
+
+        if (currentAbility != -1)
+        {
+            bool abilityTargetFound = false;
+            if (!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100) || abilityTargetFound == true)
+            {
+                return;
+            }
+            else if(hit.collider.gameObject.tag == "Knight" || hit.collider.gameObject.tag == "Boss")
+            {
+                abilities[currentAbility].Cast(hit.collider.gameObject);
+                abilityTargetFound = true;
+                targetActor = hit.collider.gameObject;
+                #if UNITY_EDITOR
+                    Debug.Log(currentAbility + " and " + hit.collider.gameObject.tag);
+                #endif
+            }
+            else if (hit.collider.gameObject.tag == "Creature" && !hit.collider.isTrigger)
+            {
+                //as this detects the child capsule collider, we must get the parent object. note that if you make changes
+                //to the creature prefab it may not work if you add more colliders as child objects with different sizes.
+                //please make those child objects' layer "Ignore Raycast"
+                abilities[currentAbility].Cast(hit.collider.gameObject.transform.parent.gameObject);
+                abilityTargetFound = true;
+                targetActor = hit.collider.gameObject.transform.parent.gameObject;
+                #if UNITY_EDITOR
+                    Debug.Log(currentAbility + " and " + hit.collider.gameObject.transform.parent.gameObject.tag);
+                #endif
+            }
+            currentAbility = -1;
+        }
+        targetActor.GetComponent<ParticleSystem>().enableEmission = true;
+    }
 }
