2f3ec2e6459e622a66ce508d86a9147f5a5f85a3
FS02 <Fs_02@mail.com>
2015-01-30 03:55:17 +0700

    Add AutoSpin Utility and TargetLock prefabs

---
+++ Assets/Scripts/Utility/AutoSpin.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Utility/AutoSpin")]
+public class AutoSpin : MonoBehaviour {
+	public float x;
+	public float y;
+	public float z;
+
+	void Update () {
+		transform.Rotate (new Vector3 (x, y, z) * Time.deltaTime);
+	}
+}
