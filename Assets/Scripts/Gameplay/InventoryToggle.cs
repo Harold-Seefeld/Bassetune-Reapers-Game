390e8fe51ee4584dbabaae3a3f7f91dcf13dd8e8
Harold <seefeld.harold@gmail.com>
2015-02-15 20:42:54 +1100

    Add base for in-game inventory
    Signed-off-by: Harold <seefeld.harold@gmail.com>

---
+++ Assets/Scripts/Gameplay/InventoryToggle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventoryToggle : MonoBehaviour {
+
+	public GameObject inventoryPanel;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetButtonDown("Inventory"))
+		{
+			if (inventoryPanel.activeInHierarchy)
+			{
+				inventoryPanel.SetActive(false);
+			}
+			else
+			{
+				inventoryPanel.SetActive(true);
+			}
+		}
+	}
+}
