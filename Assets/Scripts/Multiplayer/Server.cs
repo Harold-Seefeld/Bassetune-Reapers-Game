00e3e669cbe31e3d6a8af7e90df810de679b8f92
Harold <seefeld.harold@gmail.com>
2015-07-23 20:16:12 +1000

    add current game data script

---
+++ Assets/Scripts/Multiplayer/Server.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Server : MonoBehaviour {
+	// Contains info about each player
+	public class Player {
+		public int id;
+		public string username;
+		public string nickname;
+		public string side;
+	}
+	public Player[] players;
+	public int currentPlayerID;
+}
