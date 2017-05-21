c66e70e5244ce09cbf7ee61d91c7c6c8090fd3a6
Harold Seefeld <seefeld.harold@gmail.com>
2017-05-21 21:22:44 +1000

    [Feature] Implement Healing Ability SFX [Notice] Other effects can be easily implemented the same way as the heal. Placeholders are there for each type of effect. Delayed until proper SFX available.

--- Assets/Scripts/Multiplayer/AbilityReciever.cs
+++ Assets/Scripts/Multiplayer/AbilityReciever.cs
@@ -5,7 +5,9 @@ using SocketIO;
 public class AbilityReciever : MonoBehaviour {
 
 	private SocketIOComponent socket;
-	[SerializeField] private CharacterManager characterManager;
+
+    // Special Effect Prefabs
+    public GameObject HealEffect;
 	
 	// Use this for initialization
 	void Start () {
@@ -45,20 +47,98 @@ public class AbilityReciever : MonoBehaviour {
 
 	// Also recieve effects in this class
 	public class EffectTypes {
-		public string IncreasedRange = "I_Range";
-		public string DecreasedRange = "D_Range";
-		public string Regeneration = "Regen";
-		public string Bleed = "Bleed";
-		public string Stun = "Stun";
+        public string Heal = "H";
+        public string Regeneration = "R";
+		public string Bleed = "BL";
+		public string Stun = "ST";
+        public string Flying = "FLY";
+        public string Invisibility = "I";
+        public string Poison = "P";
+        public string Purge = "PU";
+        public string Stagger = "ST";
+        public string HalfStagger = "H";
+        public string Burn = "BU";
+        public string Acid = "A";
+        public string Freeze = "F";
+        public string WrathOfFireMiasma = "W";
 	}
-	public EffectTypes effectTypes;
+    public EffectTypes effectTypes = new EffectTypes();
 	
 	void Effect(SocketIOEvent e) {
-		string[] positiveEffects = new string[2] {effectTypes.IncreasedRange, effectTypes.Regeneration};
-		string[] negativeEffects = new string[3] {effectTypes.DecreasedRange, effectTypes.Bleed, effectTypes.Stun};
-		float characterID = e.data.GetField("i").f;
-		float abilityID = e.data.GetField("a").f;
-		string type = e.data.GetField("t").str;
- 		// TODO: Implement effects so that it's visible to the player
-	}
+		string[] positiveEffects = new string[5] {effectTypes.Purge, effectTypes.Flying, effectTypes.Invisibility, effectTypes.Heal, effectTypes.Regeneration};
+		string[] negativeEffects = new string[9] {effectTypes.Bleed, effectTypes.Stun, effectTypes.Poison, effectTypes.Stagger, effectTypes.HalfStagger, effectTypes.Burn, effectTypes.Acid,
+                                                  effectTypes.Freeze, effectTypes.WrathOfFireMiasma};
+		int characterID = (int)e.data.GetField("c").f;
+		string effectType = e.data.GetField("e").str;
+        // TODO: Implement effects so that it's visible to the player
+
+        // Find character affected
+        GameObject characterObject = null;
+        foreach (CharacterData characterData in CharacterManager.instance.characterData)
+        {
+            if (characterData.CharacterID == characterID)
+            {
+                characterObject = characterData.gameObject;
+            }
+        }
+        Vector3 halfHeight = characterObject.GetComponentInChildren<CapsuleCollider>().bounds.center;
+
+        if (effectType == effectTypes.Heal)
+        {
+            // Create effect on the character
+            Instantiate(HealEffect, halfHeight, Quaternion.identity);
+        }
+        else if (effectType == effectTypes.Regeneration)
+        {
+
+        }
+        else if (effectType == effectTypes.Bleed)
+        {
+
+        }
+        else if (effectType == effectTypes.Stun)
+        {
+
+        }
+        else if (effectType == effectTypes.Flying)
+        {
+
+        }
+        else if (effectType == effectTypes.Invisibility)
+        {
+
+        }
+        else if (effectType == effectTypes.Poison)
+        {
+
+        }
+        else if (effectType == effectTypes.Purge)
+        {
+
+        }
+        else if (effectType == effectTypes.Stagger)
+        {
+
+        }
+        else if (effectType == effectTypes.HalfStagger)
+        {
+
+        }
+        else if (effectType == effectTypes.Burn)
+        {
+
+        }
+        else if (effectType == effectTypes.Acid)
+        {
+
+        }
+        else if (effectType == effectTypes.Freeze)
+        {
+
+        }
+        else if (effectType == effectTypes.WrathOfFireMiasma)
+        {
+
+        }
+    }
 }
